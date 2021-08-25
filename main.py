81d3b5fb0ccbdcb0a8904e4b85edbab965372228
Guilherme Henrique Dummer Voss <guizvoss@gmail.com>
2021-08-24 21:56:10 -0300

    iniciando métodos

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from Contato import Contato
+from Agenda import Agenda
+from Tarefas import Tarefas
+
+class Menu:
+    def __init__(self):
+        self.contato = []
+
+
+    def Cadastrar_contato(self):
+        nome = input('Digite o nome: ')
+        telefone = input('Digite o número do telefone: ')
+        email = input('Digite o email para contato: ')
+        novo_contato = Contato(nome, telefone, email)
+        self.contato.append(novo_contato)
+
+    def listar_contato(self):
 
